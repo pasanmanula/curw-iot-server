7741046e5441c5396598306501e7def728013c67
Gihan Karunarathne <gckarunarathne@gmail.com>
2018-01-22 00:46:02 +0530

    HACK: Handle stations which are not configure with `stationId` that we wanted

--- utils/UtilStation.py
+++ utils/UtilStation.py
@@ -6,6 +6,9 @@ def get_station_hash_map(stations):
     for station in stations:
         if station.get('stationId', None) is not None:
             hash_map[station.get('stationId')] = station
+        # HACK: Handle stations which are not configure with `stationId` that we wanted
+        if station.get('station_alias', None) is not None:
+            hash_map[station.get('station_alias')] = station
 
     return hash_map
 
